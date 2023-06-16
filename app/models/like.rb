09313fb90bb84d41c186fb8b8fa04e665a5dbcf3
karan jain <karanjain2212@gmail.com>
2023-06-16 22:37:01 +0530

    created models and custom methods

---
+++ app/models/like.rb
@@ -0,0 +1,7 @@
+class Like < ApplicationRecord
+  belongs_to :user, foreign_key: :author_id
+  belongs_to :post, foreign_key: :post_id
+  def update_like_counter
+    post.update(likes_counter: post.likes.count)
+  end
+end
